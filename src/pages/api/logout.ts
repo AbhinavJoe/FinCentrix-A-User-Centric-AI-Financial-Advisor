0e8e76b73770284c9a2978a0cdafb9016e87d0d6
AbhinavJoe <abhinavjoshi102003@gmail.com>
2024-07-30 07:48:08 +0530

    conditionally adding secure flag

--- src/pages/api/logout.ts
+++ src/pages/api/logout.ts
@@ -2,9 +2,10 @@ import { NextApiRequest, NextApiResponse } from 'next';
 
 export default function handler(req: NextApiRequest, res: NextApiResponse) {
     const cookieDomain = process.env.NODE_ENV === 'production' ? `Domain=${process.env.COOKIE_DOMAIN};` : '';
+    const secureFlag = process.env.NODE_ENV === 'production' ? 'Secure' : '';
 
     res.setHeader('Set-Cookie', [
-        `token=deleted; Path=/; Max-Age=0; HttpOnly; SameSite=Strict; ${cookieDomain} Secure`,
+        `token=; Path=/; Max-Age=0; HttpOnly; SameSite=Strict; ${cookieDomain} ${secureFlag}`,
     ]);
     res.status(200).json({ message: "Logged out successfully" });
 }
