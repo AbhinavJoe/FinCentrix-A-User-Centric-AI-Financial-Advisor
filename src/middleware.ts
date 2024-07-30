3f1e88a9d8097fb2dadca08cc829f0087304bee1
AbhinavJoe <abhinavjoshi102003@gmail.com>
2024-07-30 08:29:00 +0530

    updated middleware again

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,15 +1,11 @@
 import { NextResponse, type NextRequest } from "next/server";
 
 export function middleware(request: NextRequest) {
-  // Allow the request to proceed
-  const response = NextResponse.next();
-
-  // Prevent caching of the response
-  response.headers.set('Cache-Control', 'no-store');
-
   const path = request.nextUrl.pathname.toLowerCase();
+
   const isPublicPath = ["/", "/authentication", "/authentication/login", "/authentication/signup"].includes(path);
-  let token = request.cookies.get("token");
+
+  const token = request.cookies.get("token");
 
   if (isPublicPath && token) {
     return NextResponse.redirect(new URL('/Disclaimer', request.url));
@@ -18,34 +14,8 @@ export function middleware(request: NextRequest) {
   if (!isPublicPath && !token) {
     return NextResponse.redirect(new URL("/", request.url));
   }
-
-  return response;
 }
 
 export const config = {
   matcher: ["/", "/Disclaimer", "/Dashboard", "/ChatPage", "/Signup", "/Authentication/:path*"],
 };
-
-
-
-// import { NextResponse, type NextRequest } from "next/server";
-
-// export function middleware(request: NextRequest) {
-//   const path = request.nextUrl.pathname.toLowerCase();
-
-//   const isPublicPath = ["/", "/authentication", "/authentication/login", "/authentication/signup"].includes(path);
-
-//   const token = request.cookies.get("token");
-
-//   if (isPublicPath && token) {
-//     return NextResponse.redirect(new URL('/Disclaimer', request.url));
-//   }
-
-//   if (!isPublicPath && !token) {
-//     return NextResponse.redirect(new URL("/", request.url));
-//   }
-// }
-
-// export const config = {
-//   matcher: ["/", "/Disclaimer", "/Dashboard", "/ChatPage", "/Signup", "/Authentication/:path*"],
-// };
