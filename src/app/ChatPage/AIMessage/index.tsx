85bf9c590b96b120198ef7b57eb8b319ee4a3449
AbhinavJoe <abhinavjoshi102003@gmail.com>
2024-08-02 01:26:33 +0530

    added table styling to ai response

--- src/app/ChatPage/AIMessage/index.tsx
+++ src/app/ChatPage/AIMessage/index.tsx
@@ -1,30 +1,8 @@
-// import React from 'react';
-// import { RiRobot3Fill } from 'react-icons/ri';
-// import ReactMarkdown from 'react-markdown';
-
-// interface AITextProp {
-//     text: string
-// }
-
-// const AiMessage: React.FC<AITextProp> = ({ text }) => {
-//     return (
-//         <div className="w-[60%] flex flex-col">
-//             <div className='border-2 bg-transparent border-[#43443f] rounded-xl w-fit mb-1'>
-//                 <RiRobot3Fill className='text-xl' />
-//             </div>
-//             <div className="bg-[#393937]/60 border-2 border-[#43443f] rounded-xl text-lg px-4 py-3 w-fit select-text">
-//                 <ReactMarkdown>{text}</ReactMarkdown>
-//             </div>
-//         </div >
-//     );
-// }
-
-// export default AiMessage;
-
 import React from 'react';
 import { RiRobot3Fill } from 'react-icons/ri';
 import ReactMarkdown from 'react-markdown';
 import remarkGfm from 'remark-gfm';
+import './markdown.css';
 
 interface AITextProp {
     text: string;
--- src/pages/api/logout.ts
+++ src/pages/api/logout.ts
@@ -7,16 +7,3 @@ export default function handler(req: NextApiRequest, res: NextApiResponse) {
     res.setHeader('Set-Cookie', `token=; Path=/; Max-Age=0; HttpOnly; SameSite=Strict; ${cookieDomain} ${secureFlag}`);
     res.status(200).json({ message: "Logged out successfully" });
 }
-
-
-// import { NextApiRequest, NextApiResponse } from 'next';
-
-// export default function handler(req: NextApiRequest, res: NextApiResponse) {
-//     // Check if the environment is production
-//     const cookieDomain = process.env.NODE_ENV === 'production' ? `Domain=${process.env.COOKIE_DOMAIN};` : '';
-
-//     res.setHeader('Set-Cookie', [
-//         `token=; Path=/; Expires=Thu, 01 Jan 1970 00:00:00 GMT; HttpOnly; SameSite=Strict; ${cookieDomain} Secure`,
-//     ]);
-//     res.status(200).json({ message: "Logged out successfully" });
-// }
