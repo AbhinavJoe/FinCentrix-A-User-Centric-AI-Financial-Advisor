cb7690646620572150dec844255718e6864bd29d
AbhinavJoe <abhinavjoshi102003@gmail.com>
2025-01-08 19:31:47 +0530

    fixed the login bug where it was not logging in

--- src/pages/api/logout.ts
+++ src/pages/api/logout.ts
@@ -1,9 +1,9 @@
 import { NextApiRequest, NextApiResponse } from 'next';
 
 export default function handler(req: NextApiRequest, res: NextApiResponse) {
-    // const cookieDomain = process.env.NODE_ENV === 'production' ? `Domain=${process.env.COOKIE_DOMAIN};` : '';
+    const cookieDomain = process.env.NODE_ENV === 'production' ? `Domain=${process.env.COOKIE_DOMAIN};` : '';
     const secureFlag = process.env.NODE_ENV === 'production' ? 'Secure;' : '';
 
-    // res.setHeader('Set-Cookie', `token=; Path=/; Max-Age=0; HttpOnly; SameSite=Strict; ${cookieDomain} ${secureFlag}`);
+    res.setHeader('Set-Cookie', `token=; Path=/; Max-Age=0; HttpOnly; SameSite=Strict; ${cookieDomain} ${secureFlag}`);
     res.status(200).json({ message: "Logged out successfully" });
 }
